1eb8a49a8bada786b2a5e56f07ead072e94c643e
SKVLIN1991 <s_kivlin@hotmail.co.uk>
2021-08-08 15:24:38 +0100

    rewards_page_fixed

--- client/src/containers/Rewards.js
+++ client/src/containers/Rewards.js
@@ -1,28 +1,28 @@
 import React, { useEffect, useState } from 'react';
-const Rewards = ({ category }) => {
-  const [setRewards] = useState([]);
+const Rewards = ({ }) => {
+  const [rewards, setRewards] = useState([]);
+
   useEffect(() => {
-    loadRewards(category[2].url)
-  })
-  const loadRewards = url => {
-    fetch(url)
+    fetch("http://localhost:5000/api/rewards")
       .then(res => res.json())
       .then(rewardsList => {
-        return setRewards(Rewards.feed.entry);
+        return setRewards(rewardsList);
       })
       .catch(err => console.error);
-  }
+  }, [])
   const handleSelectChange = event => {
-    loadRewards(event.target.value);
+    Rewards(event.target.value);
   }
   return (
     <>
       <h4>Rewards!</h4 >
-      < Rewards
-        handleSelectChange={handleSelectChange}
-        category={category}
-      />
+
     </>
   )
+  {
+    Rewards.map((rewards) => {
+      return <option key={rewards._id} value={rewards._id} selected={rewards._id}>{rewards.fullName}</option>;
+    })
+  }
 };
 export default Rewards;
