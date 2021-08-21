aef392e48fa9a697bd5f418eca0d060fdaad5683
Garry Hall <garrymhall@gmail.com>
2021-08-21 21:05:59 +0100

    implement a burger menu

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,22 +1,50 @@
-import React from 'react';
-import '../css/NavBar.css';
+import { useState } from "react";
+import "../css/NavBar.css";
 import { Link } from "react-router-dom";
+import { HamburgerSlider as Hamburger } from "react-animated-burgers";
 
 const NavBar = () => {
+  const [open, setOpen] = useState(false);
 
+  function toggle() {
+    setOpen(!open);
+  }
+
+  function close() {
+    setTimeout(() => {
+      setOpen(false);
+    }, 200);
+  }
+
+  function buttons(active) {
+    if (active) {
+      return (
+        <>
+          <Link to="/" onClick={() => close()}>
+            Home
+          </Link>
+          <Link to="/activities" onClick={() => close()}>
+            Give Your Time
+          </Link>
+          <Link to="/rewards" onClick={() => close()}>
+            Get Rewards
+          </Link>
+          <Link to="/myaccount" onClick={() => close()}>
+            My Account
+          </Link>
+        </>
+      );
+    }
+  }
 
   return (
-
     <div className="nav-container">
       <div className="nav-bar" id="myNavBar">
-      
-        <Link to="/" >Home</Link>
-        <Link to="/activities">Give Your Time</Link>
-        <Link to="/rewards">Get Rewards</Link>      
-        <Link to="/myaccount">My Account</Link>
+        <Hamburger isActive={open} toggleButton={toggle} barColor="white" />
+        {buttons(open)}
       </div>
     </div>
   );
-}
+};
 
 export default NavBar;
